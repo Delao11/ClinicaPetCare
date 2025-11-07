f6e0438efd1d3bc7d3e080bb67d29498d9b2ca28
Paulo De La O <paulodelao11@hotmail.com>
2025-11-07 07:57:40 -0600

    crear repositorio

---
+++ Data/MacotaRepository.cs
@@ -0,0 +1,12 @@
+﻿// Interfaz para el Repositorio 
+using ClinicaPetCare.Models;
+using System.Collections.Generic;
+
+namespace ClinicaPetCare.Data
+{
+    public interface IMascotaRepository
+    {
+        void AgregarMascota(Mascota mascota);
+        IEnumerable<Mascota> ObtenerMascotas();
+    }
+}
